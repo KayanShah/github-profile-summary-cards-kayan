06f9cd367256e9c116cc1dfcacc36c80476fbab6
Kayan Shah <kayan.shah@icloud.com>
2026-04-21 20:00:15 +0100

    Update Details

--- src/utils/file-writer.ts
+++ src/utils/file-writer.ts
@@ -87,8 +87,8 @@ function getThemeMarkdown(urlPrefix: string): string {
     let result = '';
     result += `
 [![](${urlPrefix}/0-profile-details.svg)](https://github.com/KayanShah/github-profile-summary-cards)
-[![](${urlPrefix}/1-repos-per-language.svg)](https://github.com/KayanShah/github-profile-summary-cards) [![](${urlPrefix}/2-most-commit-language.svg)](https://github.com/vn7n24fzkq/github-profile-summary-cards)
-[![](${urlPrefix}/3-stats.svg)](https://github.com/KayanShah/github-profile-summary-cards) [![](${urlPrefix}/4-productive-time.svg)](https://github.com/vn7n24fzkq/github-profile-summary-cards)
+[![](${urlPrefix}/1-repos-per-language.svg)](https://github.com/KayanShah/github-profile-summary-cards) [![](${urlPrefix}/2-most-commit-language.svg)](https://github.com/KayanShah/github-profile-summary-cards)
+[![](${urlPrefix}/3-stats.svg)](https://github.com/KayanShah/github-profile-summary-cards) [![](${urlPrefix}/4-productive-time.svg)](https://github.com/KayanShah/github-profile-summary-cards)
 `;
     return result;
 }
